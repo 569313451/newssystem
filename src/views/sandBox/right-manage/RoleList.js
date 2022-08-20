5324e3929673d21947f98173abb2140e0e30f36a
shuwo.wang@yunpengai.com <shuwo.wang@yunpengai.com>
2022-08-20 17:22:04 +0800

    rolelist get

--- src/views/sandBox/right-manage/RoleList.js
+++ src/views/sandBox/right-manage/RoleList.js
@@ -1,35 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table } from 'antd'
+import axios from 'axios'
 export default function RoleList() {
-  const [dataSource] = useState([
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号'
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号'
-    }
-  ])
+  const [dataSource,setdataSource] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:8000/roles').then(res=>{
+      console.log(res);
+      setdataSource(res.data)
+    })
+  },[])
   const columns = [
     {
       title: '姓名',
-      dataIndex: 'name',
-      key: 'name'
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age'
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address'
+      dataIndex: 'label',
+      key: 'label'
     }
   ];
   return (
