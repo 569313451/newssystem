62ce6bfd7f6de20463c88ba4e3cbf585702b26d9
shuwo.wang <shuwo.wang@yunpengai.com>
2022-09-06 10:05:25 +0800

    draft 草稿箱列表

--- src/views/sandBox/news-manage/NewsDraft.js
+++ src/views/sandBox/news-manage/NewsDraft.js
@@ -1,7 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {  Table } from 'antd'
+import axios from 'axios'
 export default function NewsDraft() {
+  const [dataSource,setdataSource] = useState([])
+  useEffect(()=>{
+    // 获取草稿箱数据
+    axios.get('http://localhost:8000/news?author=admin&auditState=0').then(res=>{
+      console.log(res.data);
+      setdataSource(res.data)
+    })
+  },[])
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id'
+    },
+    {
+      title: '标题',
+      dataIndex: 'title'
+    }
+  ];
   return (
-    <div>NewsDraft</div>
+    <div>
+      <Table columns={columns} dataSource={dataSource} rowKey={(item)=>item.id}></Table>
+
+    </div>
   )
 }
