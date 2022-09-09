9533dea754e6241d214232c152cb3a1dc996bad6
shuwo.wang <shuwo.wang@yunpengai.com>
2022-09-09 12:00:31 +0800

    zhCN

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
+import zhCN from 'antd/es/locale/zh_CN';
 import IndexRouter from './router/IndexRouter'
-
+import { ConfigProvider } from 'antd'
 function App() {
-  return <div>
+  // const localeList = []
+  return <ConfigProvider locale={zhCN}>
     <IndexRouter></IndexRouter>
-  </div>
+  </ConfigProvider>
+
 }
 export default App
